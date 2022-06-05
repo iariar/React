0264dda8e5872b09f042481291ba4fe5c1ce37fe
iariss <ennimizi@gmail.com>
2022-06-05 14:25:42 +0000

    123

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox
           placeHolder='seach a monster'
           HandleChange={e => this.setState({ searchField: e.target.value })}
